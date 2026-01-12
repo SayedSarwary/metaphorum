f148b34bae259b3cb9b26d3e9b8f846692d7dcec
Sayed <masih_sarwary@yahoo.com>
2026-01-11 23:37:55 -0800

    change host to netify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,15 @@
 //     output: "static",
 // });
 
+// import { defineConfig } from "astro/config";
+
+// export default defineConfig({
+//     site: "https://SayedSarwary.github.io",
+//     base: "/metaphorum/",
+// });
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-    site: "https://SayedSarwary.github.io",
-    base: "/metaphorum/",
+  site: "https://voluble-dango-6f9eee.netlify.app",
+  base: "/",
 });
