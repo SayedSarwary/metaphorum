7295764ad5c5443b46cd8397c94838d7c9f389a5
jethro_swan <john.waters@cooptel.net>
2026-08-22 14:11:04 +0100

    Schedule re-ordered

--- schedbuilder/orgsched.py
+++ schedbuilder/orgsched.py
@@ -8,7 +8,7 @@
 import json
 
 DATAPATH = './data/'
-schedsrc = DATAPATH + "MAN_2026_Schedule_DRAFT3_website_version.csv"
+schedsrc = DATAPATH + "MAN_2026_Schedule_DRAFT4_website_version.csv"
 
 
 schedule = []
@@ -40,71 +40,3 @@ with open(DATAPATH + 'schedule.json', "w") as f:
 #    "end_time": "17:00",
 #    "room": "Conference Venue"
 #},
-
-
-
-
-
-
-
-
-
-
-#with open(DATAPATH + 'speakers.json') as f:
-#    speakers_list = json.load(f)
-#speakers_dict = {}
-#for speaker in speakers_list:
-#    speakers_dict[speaker["id"]] = speaker["full_name"]
-#print()
-#print(speakers_dict)
-
-#with open(DATAPATH + 'talks.json') as f:
-#    talks_list = json.load(f)
-#talks_dict = {}
-#for talk in talks_list:
-#    talks_dict[talk["id"]] = talk
-#print()
-#for k in talks_dict.keys():
-#    print(
-#        talks_dict[k]["id"] + " \t " \
-#        + talks_dict[k]["speaker_id"] + " \t " \
-#        + speakers_dict[talks_dict[k]["speaker_id"]] + " \t " \
-#        + talks_dict[k]["track_id"]
-#    )
-
-
-#print(talks_dict)
-
-#with open(DATAPATH + 'tracks.json') as f:
-#    tracks_list = json.load(f)
-#print()
-#print(tracks_list)
-
-
-#themes = {}
-#for k in tracks.keys():
-#    themes["theme_name"] = tracks[
-
-#with open(DATAPATH + 'allin.csv', "w") as f:
-#for k in talks.keys():
-#    talk_id = talks["id"]
-#    speaker_id = talks["speaker_id"]
-#    speaker_name = speakers[speaker_id]
-#    track_id = talks["track_id"]
-#    track_name = tracks[]
-#    title = talks["title"]
-
-
-
-
-
-
-#{
-#    "id": "sch3",
-#    "talk_id": "",
-#    "day": "2026-09-18",
-#    "start_time": "09:00",
-#    "end_time": "17:00",
-#    "room": "Conference Venue"
-#},
-
